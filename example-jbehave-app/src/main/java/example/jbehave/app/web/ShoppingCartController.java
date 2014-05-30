918a3724a84d94e5ea95bb302f1c798082b86e73
Marcin Kuthan <marcin.kuthan@gmail.com>
2014-05-30 13:53:41 +0200

    Make application more realistic, web package added with empty controller.

---
+++ example-jbehave-app/src/main/java/example/jbehave/app/web/ShoppingCartController.java
@@ -0,0 +1,8 @@
+package example.jbehave.app.web;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ShoppingCartController {
+    // Not used in acceptance tests
+}
---
+++ example-jbehave-app/src/main/java/example/jbehave/app/web/WebConfiguration.java
@@ -0,0 +1,9 @@
+package example.jbehave.app.web;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class WebConfiguration {
+}
